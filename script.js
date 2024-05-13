3a9d746337f71fafcd4a5df4af2cf01129d2b59d
Rishabh <119670176+yom-gupta@users.noreply.github.com>
2024-05-13 12:10:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const screenSize = screen.width;
+const videoAni = document.getElementById("videoani")
+// alert(screenSize)
+
+// if(screenSize <= 600 ){
+//     videoAni.src = "Phone.mp4" ;
+// }
+//else if( screenSize <= 10 ){
+//     videoAni.src = "Pc.mp4";
+// }
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+if (isMobile) {
+    videoAni.src = "Phone.mp4";
+} else {
+    videoAni.src = "Pc.mp4";
+}
+
+function clickz(){
+    alert("you are  anime fans")
+}
+
